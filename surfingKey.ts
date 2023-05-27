d00e225922ef867f819e33e9fbe7475bfc6e5c5d
quorafind <951011105@qq.com>
2023-05-27 19:47:38 +0800

    feat: support highlight text

--- surfingKey.ts
+++ surfingKey.ts
@@ -45,6 +45,17 @@ export default class ElementMonitor {
 		overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
 		overlay.style.zIndex = '99999';
 		overlay.style.pointerEvents = 'none';
+
+		const inputDisplay = document.createElement('div');
+		inputDisplay.id = 'inputDisplay';
+		inputDisplay.style.position = 'absolute';
+		inputDisplay.style.bottom = '0';
+		inputDisplay.style.left = '50%';
+		inputDisplay.style.transform = 'translateX(-50%)';
+		inputDisplay.style.color = 'white';
+		inputDisplay.style.fontSize = '24px';
+		inputDisplay.style.zIndex = '100001';
+		overlay.appendChild(inputDisplay);
 		return overlay;
 	}
 
@@ -55,23 +66,33 @@ export default class ElementMonitor {
 			if (!element) {
 				continue;
 			}
-
+		
+			// Add all children to the queue.
 			queue.push(...Array.from(element.children));
-
-			const uniqueString = this.uniqueStrings.generateUniqueString();
-			this.elementsWithUniqueStrings.set(uniqueString, element);
-
-			const elementPosition = element.getBoundingClientRect();
-
-			const stringElement = document.createElement('span');
-			stringElement.textContent = uniqueString;
-			stringElement.style.position = 'absolute';
-			stringElement.style.top = `${elementPosition.top}px`;
-			stringElement.style.left = `${elementPosition.left}px`;
-			stringElement.style.zIndex = '100000';
-			stringElement.style.pointerEvents = 'none';
-
-			this.overlay.appendChild(stringElement);
+	
+			// Check if any of the direct children is SVG or the element itself has text content.
+			const hasSvgOrTextContentChild = Array.from(element.children).some(
+				child => child instanceof SVGSVGElement
+			) || Array.from(element.childNodes).some(
+				childNode => childNode.nodeType === Node.TEXT_NODE && childNode.textContent && childNode.textContent.trim() !== ""
+			);
+		
+			if (hasSvgOrTextContentChild) {
+				const uniqueString = this.uniqueStrings.generateUniqueString();
+				this.elementsWithUniqueStrings.set(uniqueString, element);
+		
+				const elementPosition = element.getBoundingClientRect();
+		
+				const stringElement = document.createElement('span');
+				stringElement.textContent = uniqueString;
+				stringElement.style.position = 'absolute';
+				stringElement.style.top = `${elementPosition.top}px`;
+				stringElement.style.left = `${elementPosition.left}px`;
+				stringElement.style.zIndex = '100000';
+				stringElement.style.pointerEvents = 'none';
+		
+				this.overlay.appendChild(stringElement);
+			}
 		}
 		this.doc.body.appendChild(this.overlay);
 	}
@@ -81,27 +102,47 @@ export default class ElementMonitor {
 		this.keydownHandler = (e) => {
 			e.stopPropagation();
 			e.preventDefault();
-
+	
 			if (e.key === 'Escape') {
 				this.removeOverlay();
 				return;
 			}
 
+			const inputDisplay = this.overlay.querySelector('#inputDisplay');
+			if (inputDisplay) {
+				inputDisplay.textContent = inputQueue.join('');
+			}
+
+			this.overlay.querySelectorAll('span').forEach(span => {
+				if (span.textContent && span.textContent.startsWith(inputQueue.join(''))) {
+					span.style.backgroundColor = 'yellow';
+				} else {
+					span.style.backgroundColor = '';
+				}
+			});
+	
 			const input = e.key.toUpperCase();
 			if (inputQueue.length >= 3) {
 				inputQueue.shift();
 			}
 			inputQueue.push(input);
-
+	
 			const inputString = inputQueue.join('');
 			if (this.elementsWithUniqueStrings.has(inputString)) {
-				this.elementsWithUniqueStrings.get(inputString)?.dispatchEvent(new Event('click'));
+				let elementToClick = this.elementsWithUniqueStrings.get(inputString);
+	
+				// If the element is an SVG, use its parent for the click event
+				if (elementToClick instanceof SVGSVGElement && elementToClick.parentElement) {
+					elementToClick = elementToClick.parentElement;
+				}
+				
+				elementToClick?.dispatchEvent(new Event('click'));
 				this.removeOverlay();
 			}
 		};
-
+	
 		window.addEventListener('keydown', this.keydownHandler);
-	}
+	}	
 
 	init(): void {
 		this.attachStringsToElements();
