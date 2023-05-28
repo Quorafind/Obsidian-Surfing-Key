fe87b6266831344d202a61ebf6e07ce85a48415d
Quorafind <bonopengate@gmail.com>
2023-05-28 14:56:30 +0800

    feat: support context menu

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-import { Plugin } from 'obsidian';
+import {App, Modal, Plugin } from 'obsidian';
 import ElementMonitor from "./surfingKey";
 
-export default class MyPlugin extends Plugin {
+export default class SurfingKeyPlugin extends Plugin {
 	documentMonitor: ElementMonitor;
 
 	async onload() {
@@ -9,7 +9,7 @@ export default class MyPlugin extends Plugin {
 			id: 'surfing-obsidian',
 			name: 'Surfing Obsidian',
 			callback: () => {
-				this.documentMonitor = new ElementMonitor(document);
+				this.documentMonitor = new ElementMonitor(document, this);
 				this.documentMonitor.init();
 			}
 		});
@@ -18,6 +18,33 @@ export default class MyPlugin extends Plugin {
 	onunload() {
 		if (this.documentMonitor) {
 			this.documentMonitor.removeOverlay();
+		}
+	}
+}
+
+
+export class SurfingKeyModal extends Modal {
+	private plugin: SurfingKeyPlugin;
+	private elementMonitor: ElementMonitor;
+
+	constructor(app: App, plugin: SurfingKeyPlugin, elementMonitor: ElementMonitor) {
+		super(app);
+
+		this.app = app;
+		this.plugin = plugin;
+		this.elementMonitor = elementMonitor;
+	}
+
+	onOpen() {
+		this.containerEl.addEventListener('click', ()=>{
+			this.close();
+		})
+	}
+
+	onClose() {
+		this.containerEl.empty();
+		if (!this.elementMonitor.removed) { // 添加条件检查，如果已经移除就不再调用removeOverlay
+			this.elementMonitor.removeOverlay();
 		}
 	}
 }
--- surfingKey.ts
+++ surfingKey.ts
@@ -1,46 +1,64 @@
 import { Keymap } from "obsidian";
+import SurfingKeyPlugin, {SurfingKeyModal} from "./main";
 
 class UniqueStrings {
 	private usedStrings: Set<string> = new Set();
 	private index: number = 0;
+	private secondIndex: number = 0;
 	private characters = "QWERTASDFGZXCVB";
 
 	generateUniqueString(): string {
 		let result = "";
-
-		while (this.usedStrings.has(result) || result === "") {
-			this.index++;
-			let currentIndex = this.index;
-			result = "";
-			for (let i = 0; i < 2; i++) {
-				const charIndex = currentIndex % this.characters.length;
-				result += this.characters.charAt(charIndex);
-				currentIndex = Math.floor(currentIndex / this.characters.length);
-			}
-		}
+		do {
+			const firstChar = this.characters.charAt(this.index);
+			const secondChar = this.characters.charAt(this.secondIndex);
+			result = firstChar + secondChar;
+			this.advanceIndices();
+		} while (this.usedStrings.has(result));
 
 		this.usedStrings.add(result);
 		return result;
 	}
+
+	private advanceIndices(): void {
+		this.secondIndex++;
+		if (this.secondIndex >= this.characters.length) {
+			this.secondIndex = 0;
+			this.index++;
+			if (this.index >= this.characters.length) {
+				this.index = 0; // We've exhausted all possible combinations.
+			}
+		}
+	}
 }
 
+
 export default class ElementMonitor {
+	private plugin: SurfingKeyPlugin;
+	private modal: SurfingKeyModal;
+
 	private doc: Document;
 	private overlay: HTMLElement;
 	private uniqueStrings: UniqueStrings;
 	private elementsWithUniqueStrings: Map<string, Element> = new Map();
 	private keydownHandler: ((e: KeyboardEvent) => void) | null = null;
 
-	constructor(doc: Document) {
+	removed: boolean = false;
+
+
+	constructor(doc: Document, plugin: SurfingKeyPlugin) {
 		this.doc = doc;
+		this.plugin = plugin;
 		this.uniqueStrings = new UniqueStrings();
 		this.overlay = this.createOverlay();
 	}
 
 	private createOverlay(): HTMLElement {
-		const overlay = document.body.createDiv({
-			cls: 'surfing-key-overlay',
-		});
+		this.modal = new SurfingKeyModal(app, this.plugin, this);
+		this.modal.open();
+		this.modal.containerEl.empty();
+		const overlay = this.modal.containerEl;
+		overlay.addClasses(['surfing-key-overlay']);
 
 		overlay.createDiv({
 			cls: 'inputDisplay',
@@ -49,6 +67,16 @@ export default class ElementMonitor {
 			}
 		});
 		return overlay;
+	}
+
+	private isElementInViewport(el: Element): boolean {
+		const rect = el.getBoundingClientRect();
+		return (
+			rect.top >= 0 &&
+			rect.left >= 0 &&
+			rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+			rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+		);
 	}
 
 	attachStringsToElements(): void {
@@ -61,60 +89,75 @@ export default class ElementMonitor {
 			}
 
 			const pushChildren = (child: HTMLElement) => {
+				// Get computed style of the child
+				if (child instanceof Element) {
+					const style = window.getComputedStyle(child as Element);
+					// Ignore if the display property is "none"
+					if (style.display === "none") {
+						return false;
+					}
+				}
 				queue.push(child);
-				if (child instanceof SVGSVGElement) return true;
-				return !!(child.nodeType === Node.TEXT_NODE && child.textContent?.trim() && child.textContent !== "/");
+				if (child instanceof SVGSVGElement && !child.classList?.contains('canvas-background') && !child.classList?.contains('canvas-edges')) return true;
+				else if (child.classList?.contains('canvas-node-container')) return true;
+				return !!(child.nodeType === Node.TEXT_NODE && child.textContent?.trim() && child.textContent !== "/")
 			};
+
 
 			const hasSvgOrTextContentChild = Array.from(element.childNodes).some(pushChildren);
 			if (hasSvgOrTextContentChild) {
-				const elementPosition = element.getBoundingClientRect();
-				if (elementPosition.top !== 0 || elementPosition.left !== 0) {
-					const uniqueString = this.uniqueStrings.generateUniqueString();
-					this.elementsWithUniqueStrings.set(uniqueString, element);
+				const style = window.getComputedStyle(element);
+				if (style.display !== "none" && this.isElementInViewport(element)) {
+					const elementPosition = element.getBoundingClientRect();
+					if (elementPosition.top !== 0 || elementPosition.left !== 0) {
+						const uniqueString = this.uniqueStrings.generateUniqueString();
+						this.elementsWithUniqueStrings.set(uniqueString, element);
 
-					const stringElement = this.overlay.createEl('span', {
-						cls: 'surfing-key-string',
-					});
-					stringElement.textContent = uniqueString;
+						const stringElement = this.overlay.createEl('span', {
+							cls: 'surfing-key-string',
+						});
+						stringElement.textContent = uniqueString;
 
-					const midPointX = elementPosition.left + elementPosition.width / 2;
-					const midPointY = elementPosition.top + elementPosition.height / 2;
+						const midPointX = elementPosition.left + elementPosition.width / 2;
+						const midPointY = elementPosition.top + elementPosition.height / 2;
 
-					const stringElementRect = stringElement.getBoundingClientRect();
-					const overlayRect = this.overlay.getBoundingClientRect();
-					const stringWidth = stringElementRect.width;
-					const stringHeight = stringElementRect.height;
+						const stringElementRect = stringElement.getBoundingClientRect();
+						const overlayRect = this.overlay.getBoundingClientRect();
+						const stringWidth = stringElementRect.width;
+						const stringHeight = stringElementRect.height;
 
-					const rightPosition = midPointX + stringWidth / 2;
-					const bottomPosition = midPointY + stringHeight / 2;
+						const rightPosition = midPointX + stringWidth / 2;
+						const bottomPosition = midPointY + stringHeight / 2;
 
-					if (rightPosition > overlayRect.right) {
-						stringElement.style.left = `${midPointX - stringWidth}px`;
-					} else {
-						stringElement.style.left = `${midPointX}px`;
-					}
+						if (rightPosition > overlayRect.right) {
+							stringElement.style.left = `${midPointX - stringWidth}px`;
+						} else {
+							stringElement.style.left = `${midPointX}px`;
+						}
 
-					if (bottomPosition > overlayRect.bottom) {
-						stringElement.style.top = `${midPointY - stringHeight}px`;
-					} else if (midPointY + stringHeight / 2 > overlayRect.bottom) {
-						stringElement.style.top = `${midPointY - stringHeight}px`;
-					} else {
-						stringElement.style.top = `${midPointY - 2}px`;
+						if (bottomPosition > overlayRect.bottom) {
+							stringElement.style.top = `${midPointY - stringHeight}px`;
+						} else if (midPointY + stringHeight / 2 > overlayRect.bottom) {
+							stringElement.style.top = `${midPointY - stringHeight}px`;
+						} else {
+							stringElement.style.top = `${midPointY - 2}px`;
+						}
 					}
 				}
 			}
 			processQueue(queue);
 		};
 		processQueue([this.doc.documentElement]);
-		this.doc.body.appendChild(this.overlay);
 	}
 
 
 
 	monitorUserInput(): void {
 		const inputQueue: string[] = [];
+		let isTickPressed = false;  // 初始化变量来捕获 '`' 字符输入
 		this.keydownHandler = (e) => {
+			if(!this.overlay) return;
+
 			e.stopPropagation();
 			e.preventDefault();
 
@@ -127,10 +170,20 @@ export default class ElementMonitor {
 				return;
 			}
 
+			// 判断 '`' 字符是否被按下
+			if (e.key === '`') {
+				isTickPressed = true;
+				return;
+			}
+
 			if(!(/^[qwertasdfgzxcvbQWERTASDFGZXCVB]$/i.test(e.key))) {
 				if(e.key === 'Backspace' || e.key === 'Delete') {
 					inputQueue.pop();
-					// 当删除输入的字符串后，重新显示所有隐藏的元素
+					isTickPressed = false;
+					const inputDisplay = this.overlay.querySelector('#inputDisplay');
+					if (inputDisplay) {
+						inputDisplay.textContent = inputQueue.join('');
+					}
 					this.overlay.querySelectorAll('span').forEach(span => span.show());
 				}
 				return;
@@ -155,7 +208,6 @@ export default class ElementMonitor {
 					span.style.color = 'black';
 				} else {
 					span.style.backgroundColor = '';
-					// 当输入的字符串没有命中时，隐藏相关的元素
 					span.hide();
 				}
 			});
@@ -168,28 +220,37 @@ export default class ElementMonitor {
 				}
 
 				this.removeOverlay();
-				elementToClick?.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+
+				// 当 '`' 字符被按下时，触发右键点击，否则触发左键点击
+				if (isTickPressed) {
+					elementToClick?.dispatchEvent(new MouseEvent('contextmenu', { bubbles: true, clientX: elementToClick.getBoundingClientRect().left + elementToClick.clientWidth / 2, clientY: elementToClick.getBoundingClientRect().top + elementToClick.clientHeight / 2}));
+				} else {
+					elementToClick?.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+				}
+				isTickPressed = false;  // 重置状态
 			}
 		};
 
-		window.addEventListener('keydown', this.keydownHandler);
+		window.addEventListener('keydown', this.keydownHandler, true);
 	}
+
+
 
 
 	init(): void {
 		this.attachStringsToElements();
 		this.monitorUserInput();
-		this.doc.addEventListener('click', (e) => {
-			if (e.target === this.doc.body) {
-				this.removeOverlay();
-			}
-		});
 	}
 
 	removeOverlay(): void {
-		this.overlay.remove();
+		if (this.modal && !this.removed) {
+			this.removed = true; // 设置状态为已移除，防止递归调用
+			setTimeout(()=>{
+				this.modal.close();
+			}, 0);
+		}
 		if (this.keydownHandler) {
-			window.removeEventListener('keydown', this.keydownHandler);
+			window.removeEventListener('keydown', this.keydownHandler, true);
 			this.keydownHandler = null;
 		}
 	}
